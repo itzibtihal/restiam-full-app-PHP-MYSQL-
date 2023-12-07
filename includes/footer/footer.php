64315eee7583ec13accda380a75a35a6b15db3f7
itzibtihal <ibtihalboukhanchouch@gmail.com>
2023-12-07 20:33:43 +0100

    update footer and header

---
+++ includes/footer/footer.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    
+<div id="footer">
+        <div class="container margin-container">
+            <div class="about">
+                <div class="about_header">
+                    <img src="images/favicon.png" alt="favicon">
+                    <h2 class="logo">Mealify</h2>
+                </div>
+                <p>Mealify is a restaurant landing page that offers a delicious dining experience to its customers.</p>
+                <h3>Social Media </h3>
+                <ul class="social_links">
+                    <li><a href="https://twitter.com/" target="_blank"><i class="fa-brands fa-twitter"></i></a></li>
+                    <li><a href="http://facebook.com" target="_blank"><i class="fa-brands fa-facebook-f"></i></a>
+                    </li>
+                    <li><a href="https://www.instagram.com/" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+                    </li>
+                    <li><a href="https://www.linkedin.com/feed/" target="_blank"><i
+                                class="fa-brands fa-linkedin"></i></a></li>
+                </ul>
+            </div>
+
+            <div class="subscription">
+                <h3>Subscribe our Newsletter</h3>
+                <p>Don't miss out on our latest menu updates and exclusive offers - join our newsletter today and stay
+                    up-to-date with all things Mealify!</p>
+                <div class="subscription-group">
+                    <input type="text" placeholder="Enter your Email Address">
+                    <button class="btn-main"><i class="fa-solid fa-envelope"></i>Subscribe</button>
+                </div>
+
+                <div class="quick-links">
+                    <h3>Quick links</h3>
+                    <ul>
+                        <li> <i class="fa-solid fa-chevron-right fa-xs"></i> <a href="index.html">Menu</a></li>
+                        <li> <i class="fa-solid fa-chevron-right fa-xs"></i> <a href="index.html">About Us</a></li>
+                        <li> <i class="fa-solid fa-chevron-right fa-xs"></i> <a href="index.html">Testimonial</a></li>
+                        <li> <i class="fa-solid fa-chevron-right fa-xs"></i> <a href="index.html">Promo</a></li>
+                        <li> <i class="fa-solid fa-chevron-right fa-xs"></i> <a href="index.html">Contact</a></li>
+                        <li> <i class="fa-solid fa-chevron-right fa-xs"></i> <a href="index.html">Our partners</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="contact-details">
+                <h3>Get in Touch</h3>
+                <ul>
+                    <li><i class="fa-solid fa-location-dot"></i> <span>
+                            A108 Adam Street, New York, NY 535022</li>
+                    <li><i class="fa-solid fa-envelope"></i> <span>
+                            <a href="mailto:contact@example.com">contact@example.com</a></li>
+                    <li><i class="fa-solid fa-phone"></i>
+                        <a href="tel:+1 5589 55488 55"> +1 5589 55488 55</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ includes/header/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+</head>
+<body>
+<div class="navbar">
+        <div class="container">
+            <h1 class="logo">
+                <a href="index.html">Restiam</a>
+            </h1>
+
+            <ul class="links">
+                <i class="fa-solid fa-xmark"></i>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#chefs">Chefs</a></li>
+                <li><a href="#gallery">Gallery</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="pani/menu.php">menu</a></li>
+                <li><a href="#">Login</a></li>
+            </ul>
+
+            <div class="mode">
+                <i class="toggle fa-solid fa-moon "></i>
+                <i class="fa-solid fa-bars"></i>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
